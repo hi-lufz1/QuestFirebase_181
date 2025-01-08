f6ebc30c1af4e5599e3407744123b04ccb7406fc
hi-lufz1 <latifufauzi@gmail.com>
2025-01-08 15:08:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/firebasepam/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.firebasepam.ui.viewmodel
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.firebasepam.MahasiswaApplication
+
+object PenyediaViewModel {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(MahasiswaApp().container.mahasiswaRepository) }
+    }
+
+    fun CreationExtras.MahasiswaApp(): MahasiswaApplication =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplication)
+}
