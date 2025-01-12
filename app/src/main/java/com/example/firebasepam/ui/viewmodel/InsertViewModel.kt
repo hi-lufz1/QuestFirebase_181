96de81de298a6752b9301de78a1db99732579d08
hi-lufz1 <latifufauzi@gmail.com>
2025-01-12 11:55:52 +0700

    membuat data class dan fungsi validasi data

--- app/src/main/java/com/example/firebasepam/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebasepam/ui/viewmodel/InsertViewModel.kt
@@ -3,9 +3,23 @@ package com.example.firebasepam.ui.viewmodel
 import com.example.firebasepam.model.Mahasiswa
 
 
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenis_kelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+) {
+    fun isValid(): Boolean {
+        return nim != null && nama != null && jenis_kelamin != null
+                && alamat != null && kelas != null && angkatan != null
+    }
+}
+
 
 // variabel untuk simpan data dari form input
-data class MahasiswaEvent (
+data class MahasiswaEvent(
     val nim: String = "",
     val nama: String = "",
     val jenis_kelamin: String = "",
