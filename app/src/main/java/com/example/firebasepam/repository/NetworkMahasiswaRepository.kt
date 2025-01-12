eba403cf5c8caf3bec2a390d74a14f1ce16ee7ed
hi-lufz1 <latifufauzi@gmail.com>
2025-01-12 10:34:46 +0700

    implementasi update mahasiswa

--- app/src/main/java/com/example/firebasepam/repository/NetworkMahasiswaRepository.kt
+++ app/src/main/java/com/example/firebasepam/repository/NetworkMahasiswaRepository.kt
@@ -39,7 +39,14 @@ class NetworkMahasiswaRepository(
     }
 
     override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
+        try {
+            firestore.collection("Mahasiswa")
+                .document(mahasiswa.nim)
+                .set(mahasiswa)
+                .await()
+        } catch (e: Exception) {
+            throw Exception("Gagal mengupdate data mahasiswa: ${e.message}")
+        }
     }
 
     override suspend fun deleteMahasiswa(nim: String) {
